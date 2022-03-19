4843811a4739536c5f10d398036623b2e0114291
Suleyman Oner <suleymanoner1999@gmail.com>
2022-03-19 01:10:38 +0100

    loginscreen added

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -2,34 +2,70 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, Dimensions, FlatList, TouchableOpacity, Alert, Image } from 'react-native';
 import { useNavigation } from '../utils/useNavigation'
 import { MAIN_COLOR } from '../utils/Config'
-import { TextField } from '../components/TextField';
+import { TextField } from '../components';
 
 const LoginScreen = () => {
 
     const { navigate } = useNavigation()
+    const [isSignUp, setIsSignUp] = useState(false)
 
     useEffect(() => {
         
     }, [])
 
 
-    return(
-        <View style={styles.container} >
+    const onTapGoSignUp = () => {
+        setIsSignUp(true)
+    }
 
-            <View style={styles.body} >
-                <Image source={require('../assets/images/quotes-icon.png')} style={styles.image} />
-                <Text style={styles.title} >"Quotes"</Text>
+    const onTapGoSignIn= () => {
+        setIsSignUp(false)
+    }
+
+    if(!isSignUp) {
+        return(
+            <View style={styles.container} >
+    
+                <View style={styles.body} >
+                    <Image source={require('../assets/images/quotes-icon.png')} style={styles.image} />
+                    <Text style={styles.title} >"Quotes"</Text>
+                </View>
+    
+                <View style={styles.input_container} >
+                    <TextField placeholder='email' onTextChange={() => {}} />
+                    <TextField placeholder='password' onTextChange={() => {}} isSecure={true} />
+                    <TouchableOpacity onPress={onTapGoSignUp} >
+                        <Text style={styles.link_text} >You don't have account yet? Click for Sign-up.</Text>
+                    </TouchableOpacity>
+                </View>
+    
+    
             </View>
+        )
+    } else {
+        return(
+            <View style={styles.container} >
 
-            <View style={styles.input_container} >
-                <TextField placeholder='email' onTextChange={() => {}} />
-                <TextField placeholder='password' onTextChange={() => {}} isSecure={true} />
-                <Text style={styles.link_text} >You don’t have account yet? Click for Sign-up.</Text>
+                <Text style={styles.title} >"Sign Up"</Text>
+
+                <TextField placeholder='Name' onTextChange={() => {}} />
+                <TextField placeholder='Surname' onTextChange={() => {}} />
+                <TextField placeholder='Email' onTextChange={() => {}} />
+                <TextField placeholder='Username' onTextChange={() => {}} />
+                <TextField placeholder='Password' onTextChange={() => {}} isSecure={true} />
+                <TextField placeholder='Password Again' onTextChange={() => {}} isSecure={true} />
+
+
+
+                <TouchableOpacity onPress={onTapGoSignIn} >
+                    <Text style={styles.link_text} >You don't have account yet? Click for Sign-up.</Text>
+                </TouchableOpacity>
+    
+    
             </View>
+        )
+    }
 
-
-        </View>
-    )
 }
 
 export { LoginScreen }
@@ -48,14 +84,16 @@ const styles = StyleSheet.create({
         flex: 8,
     },
     image: {
-        width: 100,
-        height: 100,
+        width: 150,
+        height: 150,
     },
     title: {
         fontSize: 45,
-        marginBottom: 50,
+        marginBottom: 15,
+        marginTop: 15,
         fontFamily: 'Aleo-Regular',
-        color: "black"
+        color: "black",
+        textAlign: "center"
     },
     link_text: {
         textAlign: "center",
